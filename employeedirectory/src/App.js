49200aacd9e363739607a4bb0a9c3bab319305c2
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-07-08 22:06:31 +0930

    created tables and employees

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,8 +1,61 @@
-import React from "react";
-import EmployeeDirectory from "./pages/EmployeeDirectory";
+import React, { Component } from "react";
+import FriendCard from "./components/EmployeeTable";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import employees from "./employees.json";
+import SearchForm from "./components/SearchForm";
+import Hero from "./components/Hero";
 
-function App() {
-  return <EmployeeDirectory />;
+
+class App extends Component {
+  // // Setting this.state.employees to the employee json array
+  // state = {
+  //   employees: ['']
+  // };
+
+  // componentDidMount = query => {
+  //   employees.search(query)
+  //     .then(res => this.setState({ employees: res.data.result }))
+  //     .catch(err => console.log(err));
+  // };
+
+  // handleInputChange = event => {
+  //   this.setState({ search: event.target.value });
+  // };
+
+  // // Map over this.state.employees and render a employeeTable component for each employee object
+
+
+  render() {
+
+    return (
+      <Wrapper>
+        <div>   
+    <Hero backgroundImage="https://images.squarespace-cdn.com/content/v1/556ea5c8e4b081da85e406c7/1509577883908-TEUQVWA2Y2HA7C50A5EB/ke17ZwdGBToddI8pDm48kNIz01GxeNglza_traDdevsUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKck_3oyjxD2E6g656zntaR4vQBwPCV_Yc9BnFEdXAQ6xzMr4wPFfphDwEQcs_2fgoE/Toggl_Blog+Illustrations_MarkConlan25?format=2500w">
+        <Title>Employee Directory</Title>
+        <h3>Click on carrots to filter by heading or use the search box to narrow your results</h3>
+    </Hero>
+</div>
+              <SearchForm               
+              handleFormSubmit={this.handleFormSubmit}
+              handleInputChange={this.handleInputChange}
+              employee={this.employee}
+              />
+        
+          
+        {this.state.employees.sorted.map(employees => (
+          <FriendCard
+            id={employees.id}
+            name={employees.name}
+            position={employees.position}
+            email={employees.email}
+            phone={employees.phone}
+            DOB={employees.dob}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
---
+++ employeedirectory/src/components/EmployeeTable.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { makeStyles, emphasize } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function EmployeeTable(props) {
+  const classes = useStyles();
+
+  return (
+
+    // Table Header
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="Table Of Employees">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Image</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Position</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Phone</TableCell>
+            <TableCell align="right">D.O.B</TableCell>
+          </TableRow>
+        </TableHead>
+    
+        <TableBody>
+          {employee.sort.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+                </TableCell>
+
+            <TableCell><div className="img-container"><img alt={props.name} src={props.image} /></div>
+            <TableCell align="right">{props.name}</TableCell>
+            <TableCell align="right">{props.position}</TableCell>
+            <TableCell align="right">{props.email}</TableCell>
+            <TableCell align="right">{props.phone}</TableCell>
+            <TableCell align="right">{props.dob}</TableCell>
+            </TableCell>
+            </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default EmployeeTable;
