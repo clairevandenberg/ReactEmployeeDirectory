2ec13b0648a3733eb5ad44b55a1c476719c56e3f
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-07-08 23:12:31 +0930

    debugging

--- employeedirectory/src/components/sorting.js
+++ employeedirectory/src/components/sorting.js
@@ -3,11 +3,9 @@ import React from 'react';
 const Sorting = (props) => {
   return (
     <div>
-    <Wrapper>
             <button className="button" onClick={()=>this.sort('ascending')}>Sort by Accencding </button>
             <button className="button" onClick={()=>this.sort('descending')}>Sort by Descending </button>
-            </Wrapper> 
-                    </div>
+    </div>
     )
   };
   
